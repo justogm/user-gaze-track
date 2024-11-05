6603bd4a40dbe535dc760fb53739bb30fb04e84d
Justo <justogarciapc@gmail.com>
2024-11-05 11:12:51 -0300

    Modifico modelos eliminando trayectoria ya que se tomara cada sesión de un usuario como una entidad

--- app/models.py
+++ app/models.py
@@ -7,19 +7,12 @@ class Sujeto(db.Model):
     nombre = db.Column(db.String(50), nullable=False)
     apellido = db.Column(db.String(50), nullable=False)
     edad = db.Column(db.Integer, nullable=False)
-    trayectoria_rel = db.relationship('Trayectoria', backref='sujeto', lazy=True, uselist=False)
-
-class Trayectoria(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sujeto_id = db.Column(db.Integer, db.ForeignKey('sujeto.id'), nullable=False)
 
 class Punto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     x = db.Column(db.Float, nullable=False)
     y = db.Column(db.Float, nullable=False)
-    trayectoria_id = db.Column(db.Integer, 
-                               db.ForeignKey('trayectoria.id'), 
-                               nullable=False)
+    sujeto_id = db.Column(db.Integer, db.ForeignKey('sujeto.id'), nullable=False)
 
     def __str__(self):
-        return f'Punto ({self.coordenada_x}, {self.coordenada_y})'
+        return f'Punto ({self.x}, {self.y})'
