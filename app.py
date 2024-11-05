c91b15323402c5bfe69ef0ae9c4a85ba793ec46e
Justo <justogarciapc@gmail.com>
2024-11-05 11:12:09 -0300

    Añado modificaciones para redireccionar a las dos páginas de visualización de resultados y modificaciones en la inserción de datos

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
-from app.models import db, Sujeto, Trayectoria, Punto
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from app.models import db, Sujeto, Punto
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -7,11 +7,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance', 'usergazetrack.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+                                        os.path.join(basedir, 'instance', 'usergazetrack.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,22 +18,47 @@ def index():
         nombre = request.form['nombre']
         apellido = request.form['apellido']
         edad = request.form['edad']
-        return redirect(url_for('embed', nombre=nombre, apellido=apellido, edad=edad))
+
+        sujeto = Sujeto(nombre=nombre, apellido=apellido, edad=edad)
+        db.session.add(sujeto)
+        db.session.commit()
+
+        return redirect(url_for('embed', id=sujeto.id))
     return render_template('index.html')
 
 @app.route('/gaze-tracking')
 def embed():
-    nombre = request.args.get('nombre')
-    apellido = request.args.get('apellido')
-    edad = request.args.get('edad')
+    return render_template('embed.html', id=request.args.get('id'))
 
-    trayectoria = Trayectoria()
-    db.session.add(trayectoria)
-    sujeto = Sujeto(nombre=nombre, apellido=apellido, edad=edad, trayectoria_rel=trayectoria)
-    db.session.add(sujeto)
+
+@app.route('/sujetos')
+def sujetos():
+    sujetos_db = Sujeto.query.all()
+    return render_template('sujetos.html', sujetos=sujetos_db)
+
+@app.route('/resultados')
+def resultados():
+    sujeto_id = request.args.get('id')
+
+    sujeto = Sujeto.query.filter_by(id=sujeto_id).first()
+
+    if sujeto:
+        puntos = Punto.query.filter_by(sujeto_id=sujeto.id).all()
+        return render_template('resultados.html', sujeto=sujeto, puntos=puntos)
+
+    return "Sujeto no encontrado", 404
+
+@app.route('/guardar-puntos', methods=['POST'])
+def guardar_puntos():
+    data = request.get_json()
+    puntos = data['puntos']
+
+    for punto in puntos:
+        nuevo_punto = Punto(x=punto['x'], y=punto['y'], sujeto_id=data['id'])
+        db.session.add(nuevo_punto)
     db.session.commit()
+    return jsonify({'status': 'success'})
 
-    return render_template('embed.html', nombre=nombre, apellido=apellido, edad=edad)
 
 if __name__ == '__main__':
     with app.app_context():
