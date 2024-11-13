903f497068dac0ce9254afff1d3e9711904efbc8
Justo <justogarciapc@gmail.com>
2024-11-13 12:01:46 -0300

    Añado ruta para descargar la información de los puntos para cada usuario y la documentación para swagger

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, send_from_directory, send_file
-from app.models import db, Sujeto, Punto
-from flask_sqlalchemy import SQLAlchemy
-from flasgger import Swagger
+"""
+Module docstring TODO: completar
+"""
 import os
 import csv
 import io
+from flask import Flask, render_template, request, redirect, url_for, jsonify, send_from_directory, send_file
+import numpy as np
+from flask_sqlalchemy import SQLAlchemy
+from flasgger import Swagger
+from app.models import db, Sujeto, Punto
+
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -20,8 +26,7 @@ swagger = Swagger(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """
-    Página principal que permite el registro de un nuevo sujeto para la medición de su \
-        mirada.
+    Página principal que permite el registro de un nuevo sujeto para la medición de su mirada.
     ---
     parameters:
       - name: nombre
@@ -75,11 +80,34 @@ def embed():
 
 @app.route('/sujetos')
 def sujetos():
+    """
+    Muestra la lista de sujetos registrados en la base de datos.
+    ---
+    responses:
+        200:
+            description: Página con la lista de sujetos registrados.
+    """
     sujetos_db = Sujeto.query.all()
     return render_template('sujetos.html', sujetos=sujetos_db)
 
 @app.route('/resultados')
 def resultados():
+    """
+    Muestra los resultados de los puntos registrados para un sujeto en particular y \
+        permite su descarga.
+    ---
+    parameters:
+        - name: id
+          in: query
+          type: integer
+          required: true
+          description: ID del sujeto para mostrar resultados.
+    responses:
+        200:
+            description: Página con los resultados de los puntos registrados.
+        404:
+            description: Sujeto no encontrado.
+    """
     sujeto_id = request.args.get('id')
 
     sujeto = Sujeto.query.filter_by(id=sujeto_id).first()
@@ -93,6 +121,31 @@ def resultados():
 
 @app.route('/guardar-puntos', methods=['POST'])
 def guardar_puntos():
+    """
+    Guarda los puntos registrados en la base de datos.
+    ---
+    parameters:
+        - name: puntos
+          in: body
+          required: true
+          schema:
+            type: object
+            properties:
+                id:
+                    type: integer
+                puntos:
+                    type: array
+                    items:
+                        type: object
+                        properties:
+                            x:
+                                type: number
+                            y:
+                                type: number
+    responses:
+        200:
+            description: status success
+    """
     data = request.get_json()
     puntos = data['puntos']
 
@@ -104,11 +157,33 @@ def guardar_puntos():
 
 @app.route('/config')
 def config():
+    """
+    Descarga el archivo de configuración.
+    ---
+    responses:
+        200:
+            description: Archivo de configuración.
+    """
     return send_from_directory('config', 'config.json')
 
 
 @app.route('/descargar-puntos')
 def descargar_puntos():
+    """
+    Descarga los puntos registrados para un sujeto en particular.
+    ---
+    parameters:
+        - name: id
+          in: query
+          type: integer
+          required: true
+          description: ID del sujeto para descargar los puntos.
+    responses:
+        200:
+            description: Archivo CSV con los puntos registrados.
+        404:
+            description: Sujeto no encontrado.
+    """
     sujeto_id = request.args.get('id')
 
     sujeto = Sujeto.query.filter_by(id=sujeto_id).first()
@@ -124,7 +199,8 @@ def descargar_puntos():
 
         si.seek(0)
 
-        si_bytes = io.BytesIO(si.getvalue().encode('utf-8')) #Tiene que estar en este formato para send_file
+        si_bytes = io.BytesIO(si.getvalue().encode('utf-8')) \
+                                            #Tiene que estar en este formato para send_file
 
         return send_file(
             si_bytes,
@@ -134,6 +210,49 @@ def descargar_puntos():
         )
 
     return "Sujeto no encontrado", 404
+
+@app.route('/descargar-todos')
+def descargar_todos():
+    """
+    Descarga los puntos registrados para todos los sujetos en formato csv.
+    ---
+    responses:
+        200:
+            description: Archivo CSV con los puntos registrados para todos los sujetos.
+        404:
+            description: No hay sujetos registrados.
+    """
+    all_sujetos = Sujeto.query.all()
+
+    si = io.StringIO()
+    escritor_csv = csv.DictWriter(si, fieldnames=['id', 'x', 'y'])
+    escritor_csv.writeheader()
+
+    if len(sujetos) == 0:
+        return "No hay sujetos registrados", 404
+
+    for sujeto in all_sujetos:
+        puntos = Punto.query.filter_by(sujeto_id=sujeto.id).all()
+        puntos_dict = [punto.__json__() for punto in puntos]
+
+        id_sujeto = int(sujeto.id)
+
+        puntos_np = np.array([[id_sujeto, p['x'], p['y']] for p in puntos_dict])
+
+        for punto in puntos_np:
+            escritor_csv.writerow({'id': punto[0], 'x': punto[1], 'y': punto[2]})
+
+
+    si.seek(0)
+
+    si_bytes = io.BytesIO(si.getvalue().encode('utf-8'))
+    return send_file(
+        si_bytes,
+        as_attachment=True,
+        download_name="puntos_todos.csv",
+        mimetype='text/csv'
+    )
+
 
 if __name__ == '__main__':
     with app.app_context():
